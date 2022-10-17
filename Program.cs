ad87fa2471c93d6b732c8bad74055b28bb5917a4
Alex <automex74@gmail.com>
2022-10-17 21:47:13 +0500

    add task_36

--- Program.cs
+++ Program.cs
@@ -18,6 +18,26 @@ int FindEvenNumbers(int[] array)
     return count;
 }
 
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("--------------Задача №36.--------------");
+int[] arr = GetArray(6, -100, 100);
+Console.WriteLine(String.Join(", ", arr));
+Console.WriteLine($"{SumOddNumber(arr)}");
+
+int SumOddNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
